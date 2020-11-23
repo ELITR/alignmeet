dae05478c5c5b9041e3fd814ee13779b5c181b9c
Peter Polak <polakpe7@gmail.com>
2020-11-23 08:55:55 +0100

    Windows Qt fix

--- run.py
+++ run.py
@@ -3,6 +3,13 @@ import sys
 import struct
 from sys import argv
 
+if os.name == 'nt':
+    import PySide2
+
+    dirname = os.path.dirname(PySide2.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 from PySide2.QtCore import Qt, QCoreApplication
 from settings import Settings
 
